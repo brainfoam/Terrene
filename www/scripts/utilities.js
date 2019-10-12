e4f8f0870521c814e60e31daca5a19e500469337
Joe Aquiare <brainfoammgmt@gmail.com>
2019-10-12 03:05:29 -0400

    Updated page loading and external care.

--- www/scripts/utilities.js
+++ www/scripts/utilities.js
@@ -2,3 +2,23 @@ function lerp(v0, v1, t)
 {
     return(1-t)*v0+t*v1;
 }
+
+function animate(element, type) {
+    element.addClass("animate");
+    element.addClass(type);
+}
+
+function removeAnimation(element, type = undefined) {
+    element.removeClass("animate");
+    if(type != undefined)
+        element.removeClass(type);
+}
+
+function clearAppBody() {
+    document.querySelector("#app_body").innerHTML = "";
+}
+
+function loadPage(pageName) {
+    clearAppBody();
+    $("#app_body").load("pages/" + pageName + ".html");
+}
