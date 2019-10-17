75e386295502a57d5384589ef1f8b16845e1d572
Joe Aquiare <brainfoammgmt@gmail.com>
2019-10-17 18:10:05 -0400

    Updated planet visual FX

--- www/scripts/pixi_utilities.js
+++ www/scripts/pixi_utilities.js
@@ -5,9 +5,40 @@ function PIXI_makeSprite(path, stage) {
     return s;
 }
 
+function PIXI_makeText(text, style, stage) {
+    let t = new PIXI.Text(text, style);  
+    t.anchor.set(0.5);
+    stage.addChild(t);
+    return t;
+}
+
 function PIXI_copyTransform(from, to) {
     to.width  = from.width;
     to.height = from.height;
     to.x      = from.x;
     to.y      = from.y;
+}
+
+function PIXI_translate(obj, x, y) {
+    obj.x = x;
+    obj.y = y;
+}
+
+function PIXI_resize(obj, width, height) {
+    obj.width = width;
+    obj.height = height;
+}
+
+function PIXI_alterFX(obj, alpha, rotation) {
+    obj.alpha = alpha;
+    obj.rotation = rotation;
+}
+
+function PIXI_setAttributes(obj, x, y, width, height, alpha = -1, rotation = -999) {
+    PIXI_translate(obj, x, y);
+    PIXI_resize(obj, width, height);
+    if(alpha != -1)
+        PIXI_alterFX(obj, alpha, obj.rotation);
+    if(rotation != -999)
+        PIXI_alterFX(obj, obj.alpha, rotation);
 }
